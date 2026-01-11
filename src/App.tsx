e18b179809dd7a2abe2e7693bc405ddf6bc2340c
Ahmad <ahmadIstanbuli179@gmail.com>
2026-01-11 16:56:28 +0300

    Before Add Inputs UI In Headless Components Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
+import { useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard";
 import { productList } from "./components/data";
+import Modal from "./components/ui/Modal";
+import Button from "./components/ui/Button";
 
 const App = () => {
+  // State
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Handler
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
   // Render Product  List
   const renderProductList = productList.map((product) => (
     <ProductCard key={product.id} product={product}></ProductCard>
   ));
 
   return (
-    <main className="container mx-auto">
-      <div className="grid gap-4 m-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <main className="container mx-auto px-11">
+      <Button
+        width="w-fit"
+        className="bg-fuchsia-700 hover:bg-fuchsia-800"
+        onClick={open}
+      >
+        Add
+      </Button>
+      <div className="grid gap-4 mt-5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {renderProductList}
       </div>
+      <Modal isOpen={isOpen} closeModal={close} title="Product Modal">
+        <div className="flex gap-2">
+          <Button className=" bg-fuchsia-700 hover:bg-fuchsia-800">
+            Submit
+          </Button>
+          <Button onClick={close} className=" bg-gray-200 hover:bg-gray-300">
+            Cancel
+          </Button>
+        </div>
+      </Modal>
     </main>
   );
 };
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,39 @@
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+
+interface IProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  title?: string;
+  children: React.ReactNode;
+}
+
+const Modal = ({ isOpen, closeModal, title, children }: IProps) => {
+  return (
+    <>
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={closeModal}
+        __demoMode
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="transition-all flex bg-black/20 min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-white shadow p-6 duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            >
+              <DialogTitle as="h3" className="font-medium text-black text-2xl">
+                {title}
+              </DialogTitle>
+
+              <div className="mt-4">{children}</div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default Modal;
